6f6116deae909b78cef4216695aff1b8dc62a873
Omar Suniaga <omareduardo74@gmail.com>
2025-01-14 06:20:24 -0400

    chore: update node version and build configuration

--- src/types/heroicons.d.ts
+++ src/types/heroicons.d.ts
@@ -1,15 +1,22 @@
 declare module '@heroicons/vue/24/solid' {
-  const content: any
-  export const ChevronLeftIcon: any
-  export const ChevronRightIcon: any
-  // Agrega aquí otros iconos que uses
-  export default content
+  const content: any;
+  export const ChevronLeftIcon: any;
+  export const ChevronRightIcon: any;
+  export default content;
 }
 
 declare module '@heroicons/vue/24/outline' {
-  const content: any
-  export const ChevronDownIcon: any
-  export const UserCircleIcon: any
-  // Agrega aquí otros iconos que uses
-  export default content
+  const content: any;
+  export const ChevronDownIcon: any;
+  export const ArrowRightOnRectangleIcon: any;
+  export const ArrowPathIcon: any;
+  export const TrashIcon: any;
+  export const UserCircleIcon: any;
+  export const SpeakerWaveIcon: any;
+  export const BellIcon: any;
+  export const ClockIcon: any;
+  export const CalendarIcon: any;
+  export const ChartBarIcon: any;
+  export const UserIcon: any;
+  export default content;
 }
